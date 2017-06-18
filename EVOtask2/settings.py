e21f54e95b5d78206c44dd551ba792f1ddf24b75
Vitaly <vitalyturkov@gmail.com>
2017-06-19 01:25:27 +0300

    debug False

--- EVOtask2/settings.py
+++ EVOtask2/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')f6$rw__507f_z77%1=u_q3h=&iqrw%l=d65)#0v!-yw=)n-5d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
