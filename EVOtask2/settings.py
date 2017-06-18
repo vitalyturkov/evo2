65d8d0bf4b29fd690dbb0376085d9ca0d682e3f7
Vitaly <vitalyturkov@gmail.com>
2017-06-18 22:42:25 +0300

    db changes

--- EVOtask2/settings.py
+++ EVOtask2/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'EVOtask2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'filedata',
+        'USER':'ram64',
+        'PASSWORD':'123456',
+        'HOST':'ram64.mysql.pythonanywhere-services.com',
+        'PORT':3306,        
     }
 }
 
